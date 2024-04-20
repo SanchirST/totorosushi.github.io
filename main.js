0c76c67dec49ce2b58899b06ea415e33d6558375
Sanchir <154678722+SanchirST@users.noreply.github.com>
2024-04-20 16:43:32 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+var orderButton = document.getElementById("order");
+
+orderButton.addEventListener("click", function() {
+    window.location.href = "beställa.html"; // Redirect to YouTube.com
+});
